b88a54d0bc4efff83612a8ce977fc4d7386f53dc
Yohann <73314529+yohann69@users.noreply.github.com>
2024-05-22 10:57:44 +0200

    Better ui

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![cfg_attr(
+    not(debug_assertions),
+    windows_subsystem = "windows"
+)] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
 use egui_extras::{Column, TableBuilder};
 
 fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    env_logger::init();
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_min_inner_size([1100.0, 700.0]),
         ..Default::default()
     };
     eframe::run_native(
         "Universal Rust Server Software",
         options,
-        Box::new(|cc| {
-            // This gives us image support:
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-
+        Box::new(|_| {
             Box::<MyApp>::default()
         }),
     )
@@ -53,6 +53,7 @@ impl eframe::App for MyApp {
                 .body(|mut body| {
                     let players = vec!["Lopi5555", "Hethan_hdb", "Vavaaaaaah"];
                     let scores = vec!["5", "19", "0"];
+                    let padding = 10.0; // Define the left padding
 
                     for (index, (player, score)) in players.iter().zip(scores.iter()).enumerate() {
                         body.row(30.0, |mut row| {
@@ -63,11 +64,17 @@ impl eframe::App for MyApp {
                             };
                             row.col(|ui| {
                                 ui.painter().rect_filled(ui.max_rect(), 0.0, bg_color);
-                                ui.label(*player);
+                                ui.horizontal_centered(|ui| {
+                                    ui.add_space(padding); // Add left padding
+                                    ui.label(*player);
+                                });
                             });
                             row.col(|ui| {
                                 ui.painter().rect_filled(ui.max_rect(), 0.0, bg_color);
-                                ui.label(*score);
+                                ui.horizontal_centered(|ui| {
+                                    ui.add_space(padding); // Add left padding
+                                    ui.label(*score);
+                                });
                             });
                         });
                     }
@@ -75,4 +82,3 @@ impl eframe::App for MyApp {
         });
     }
 }
-
