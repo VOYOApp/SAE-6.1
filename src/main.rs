91097a1f0bc0464aa9eb8698006502f3a21959ee
Ethan Bourguigneau <ethan.devfs@pm.me>
2024-06-13 10:03:07 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,13 @@ pub mod types;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //  STARTING UI AND MOVEMENTS
-    App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, (spawn_camera, spawn_player, spawn_boundaries).chain())
-    .add_systems(Update, player_mov)
-    .add_systems(Update, block_players_in_bound.after(player_mov))
-    .add_systems(Update, resize_boundaries)
-    .run();
+    // App::new()
+    // .add_plugins(DefaultPlugins)
+    // .add_systems(Startup, (spawn_camera, spawn_player, spawn_boundaries).chain())
+    // .add_systems(Update, player_mov)
+    // .add_systems(Update, block_players_in_bound.after(player_mov))
+    // .add_systems(Update, resize_boundaries)
+    // .run();
 
     // Shared state for messages
     let messages = Arc::new(Mutex::new(Vec::new()));
@@ -35,26 +35,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_settings = Arc::clone(&settings);
 
     // Start the server in a separate thread
-    thread::spawn(move || {
-        let serv = ServerThread::new(6969, server_messages, server_settings);
-        serv.start();
-    });
+    // thread::spawn(move || {
+    //     let serv = ServerThread::new(6969, server_messages, server_settings);
+    //     serv.start();
+    // });
 
+    // Run the GUI
     // let options = eframe::NativeOptions::default();
     // eframe::run_native(
     //     "Physics Simulation",
     //     options,
     //     Box::new(|_cc| Box::new(GameUI::default())),
     // ).expect("TODO: panic message");
+    //
+    // // Run the GUI in the main thread
+    // let native_options = eframe::NativeOptions::default();
+    // eframe::run_native(
+    //     "Server GUI",
+    //     native_options,
+    //     Box::new(|_cc| Box::new(ui::server_ui::ServerUi::new(messages, settings))),
+    // ).expect("Failed to run server GUI");
 
-    // Run the GUI in the main thread
+    let thread1 = thread::spawn(|| {
+        let serv = ServerThread::new(6969, server_messages, server_settings);
+        serv.start();
+    });
+
+    let thread2 = thread::spawn(|| {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Server GUI",
         native_options,
         Box::new(|_cc| Box::new(ui::server_ui::ServerUi::new(messages, settings))),
     ).expect("Failed to run server GUI");
+    });
+
+    let thread3 = thread::spawn(|| {
+        let options = eframe::NativeOptions::default();
+        eframe::run_native(
+            "Physics Simulation",
+            options,
+            Box::new(|_cc| Box::new(GameUI::default())),
+        ).expect("TODO: panic message");
+    });
+
+    thread1.join().unwrap();
+    thread2.join().unwrap();
+    thread3.join().unwrap();
 
     Ok(())
-
 }
